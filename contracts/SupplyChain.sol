25e196e311828fd14a0170273fab4ccbaec0dba4
Qian Dong <qdong327@gmail.com>
2021-10-02 15:57:16 -0400

    owner, skucount, enum, items (partial) tests are passing

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -4,15 +4,27 @@ pragma solidity >=0.5.16 <0.9.0;
 contract SupplyChain {
 
   // <owner>
-
+  address public owner = msg.sender;
   // <skuCount>
-
+  uint public skuCount;
   // <items mapping>
-
+  mapping(address => uint) public items;
   // <enum State: ForSale, Sold, Shipped, Received>
-
+  enum State {
+    ForSale,
+    Sold,
+    Shipped,
+    Received
+  }
   // <struct Item: name, sku, price, state, seller, and buyer>
-  
+  struct Item {
+    string name;
+    uint sku;
+    uint price;
+    uint state; //Tests up to here passing
+    address seller;
+    address buyer;
+  }
   /* 
    * Events
    */
